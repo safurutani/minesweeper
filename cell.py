f3db445d05e15746075a3dba86c8518606ca221b
saf <bluesara24@gmail.com>
2022-09-17 16:20:50 -0700

    Visual feedback when mines get clicked - test

--- cell.py
+++ cell.py
@@ -1,5 +1,6 @@
 from tkinter import Button
 import random
+import settings
 
 class Cell:
     all=[]
@@ -16,22 +17,25 @@ class Cell:
             location,
             width=12,
             height=4,
-            text=f'{self.x},{self.y}'
+            text=f''
         )
         btn.bind('<Button-1>', self.lmb_actions)
         btn.bind('<Button-3>', self.rmb_actions)
         self.cell_btn_object = btn
     def lmb_actions(self, event):
-        print(event)
-        print("I got left clicked!")
+        if self.is_mine:
+            self.show_mine()
+    #ends game - player loses
+    def show_mine(self):
+        self.cell_btn_object.configure(bg="red")
     def rmb_actions(self, event):
-        print(event)
-        print("I got right clicked!")
+        print("rmb")    
 
     @staticmethod
     def randomize_mines(): 
-        mines = random.sample(Cell.all, 9)
+        mines = random.sample(Cell.all, settings.NUM_MINES)
         for picked_cell in mines:
             picked_cell.is_mine = True
+        print(mines)
     def __repr__(self):
         return f"Cell ({self.x}, {self.y})"
