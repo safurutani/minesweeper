d80c83de62360666f76b87d645ed0dc0bc084bb9
saf <bluesara24@gmail.com>
2022-09-17 13:03:15 -0700

    Created cell function and implemented it into the main

--- minesweeper.py
+++ minesweeper.py
@@ -1,6 +1,7 @@
 from tkinter import *
 import settings
 import utils
+from cell import Cell
 
 #creates the window
 root = Tk()
@@ -15,9 +16,23 @@ top_frame = Frame(
     root, 
     bg="#777AAA",
     width=800,
-    height=utils.height_percent(20)
+    height=utils.height_percent(15)
 )
 top_frame.place(x=0, y=0)
+
+center_frame = Frame(
+    root,
+    bg="#C2EAFC",
+    width=utils.width_percent(90),
+    height=utils.height_percent(75)
+)
+center_frame.place(x=utils.width_percent(5), y=utils.height_percent(20))
+
+c1 = Cell()
+c1.create_btn_object(center_frame)
+c1.cell_btn_object.place(
+    x=0, y=0
+)
 
 #keeps window open until closed out with the X
 root.mainloop()
