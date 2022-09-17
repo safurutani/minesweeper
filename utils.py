509b88dde88f9db1d91c53cad3bbf255505305ee
saf <bluesara24@gmail.com>
2022-09-17 12:45:32 -0700

    Added width percentage capabilities

--- utils.py
+++ utils.py
@@ -2,4 +2,7 @@ import settings
 
 def height_percent(percentage):
     return (settings.HEIGHT / 100) * percentage
-print(height_percent(25))
+
+def width_percent(percentage):
+    return (settings.WIDTH / 100) * percentage
+print(width_percent(25))
