b214b6b2094c90a8cbd4eadc3f05137e0d41763d
saf <bluesara24@gmail.com>
2022-09-17 16:05:55 -0700

    Randomly picked which cells are mines

--- cell.py
+++ cell.py
@@ -1,13 +1,22 @@
 from tkinter import Button
+import random
 
 class Cell:
-    def __init__(self, is_mine=False):
+    all=[]
+    def __init__(self,x, y, is_mine=False):
         self.is_mine = is_mine
         self.cell_btn_object = None
+        self.x = x
+        self.y = y
+
+        #append obj to the cell.all list
+        Cell.all.append(self)
     def create_btn_object(self, location):
         btn = Button(
             location,
-            text='Text'
+            width=12,
+            height=4,
+            text=f'{self.x},{self.y}'
         )
         btn.bind('<Button-1>', self.lmb_actions)
         btn.bind('<Button-3>', self.rmb_actions)
@@ -18,3 +27,11 @@ class Cell:
     def rmb_actions(self, event):
         print(event)
         print("I got right clicked!")
+
+    @staticmethod
+    def randomize_mines(): 
+        mines = random.sample(Cell.all, 9)
+        for picked_cell in mines:
+            picked_cell.is_mine = True
+    def __repr__(self):
+        return f"Cell ({self.x}, {self.y})"
