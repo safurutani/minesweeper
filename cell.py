6d499d4bbc2d7af60b5ab5aa4cb0cbadba6e8a70
saf <bluesara24@gmail.com>
2022-09-17 15:45:59 -0700

    Added right mouse click function

--- cell.py
+++ cell.py
@@ -10,7 +10,11 @@ class Cell:
             text='Text'
         )
         btn.bind('<Button-1>', self.lmb_actions)
+        btn.bind('<Button-3>', self.rmb_actions)
         self.cell_btn_object = btn
     def lmb_actions(self, event):
         print(event)
         print("I got left clicked!")
+    def rmb_actions(self, event):
+        print(event)
+        print("I got right clicked!")
